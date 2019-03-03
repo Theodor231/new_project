1bd251cac7ecf968793c96b56d48773feb1e0d5d
Teișanu Tudor <teisanutudort@gmail.com>
2019-03-03 23:05:29 +0300

    fix request error

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
