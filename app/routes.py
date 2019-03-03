9a81fb6b615a9f0c690699e25587ac991b44b5d4
Teișanu Tudor <teisanutudort@gmail.com>
2019-03-03 20:30:36 +0300

    connect DB

--- app/routes.py
+++ app/routes.py
@@ -23,11 +23,22 @@ def index():
     return render_template('index.html', title="Home", user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('/index'))
     return render_template('login.html', title='Sing In', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test psot #1'},
+        {'authot': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
